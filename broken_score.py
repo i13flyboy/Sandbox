e9cd3ea5ed9287a16cf5b8460cf7224bd14bae24
13flyboy <kyle.kunz@my.jcu.edu.au>
2020-08-27 15:38:37 +1000

    here are my py files

---
+++ broken_score.py
@@ -0,0 +1,17 @@
+def main():
+    score = float(input("Enter score: "))
+    print(determine_status(score))
+
+def determine_status(score):
+    """Determine the status of a given score."""
+    if score < 0 or score > 100:
+        return "Invalid score"
+    elif score >= 90:
+        return "Excellent"
+    elif score >= 50:
+        return "Passable"
+    else:
+        return "Bad"
+
+
+main()
